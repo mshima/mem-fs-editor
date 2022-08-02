77051a3877dd1d733e2a61efc23538ec22777e0d
Marcelo Shima <marceloshima@gmail.com>
2022-08-01 22:31:14 -0300

    update file permission on commit

--- __tests__/commit-file-async.js
+++ __tests__/commit-file-async.js
@@ -6,14 +6,19 @@ const path = require('path');
 const memFs = require('mem-fs');
 const sinon = require('sinon');
 const editor = require('..');
+const { STATE, STATE_MODIFIED, STATE_DELETED } = require('../lib/state');
 
 const rmSync = filesystem.rmSync || filesystem.rmdirSync;
 
-describe('#commit()', () => {
-  const outputRoot = path.join(os.tmpdir(), 'mem-fs-editor-test');
+// Permission mode are handled differently by windows, skip it.
+const posixIt = process.platform === 'win32' ? it.skip : it;
+
+describe('#commitFileAsync()', () => {
+  const outputRoot = path.join(os.tmpdir(), 'mem-fs-editor-test' + Math.random());
   const outputDir = path.join(outputRoot, 'output');
   const filename = path.join(outputDir, 'file.txt');
   const filenameNew = path.join(outputDir, 'file-new.txt');
+  let newFile;
 
   let store;
   let fs;
@@ -25,25 +30,106 @@ describe('#commit()', () => {
     fs = editor.create(store);
     fs.write(filename, 'foo');
 
+    newFile = {
+      path: filenameNew,
+      contents: Buffer.from('bar'),
+      [STATE]: STATE_MODIFIED,
+    };
+
     expect(store.add.callCount).toEqual(1);
   });
 
   afterEach(() => {
-    rmSync(outputRoot, { recursive: true });
+    if (filesystem.existsSync(outputRoot)) {
+      rmSync(outputRoot, { recursive: true });
+    }
   });
 
-  it('triggers callback when done', async () => {
+  it('commits a modified file', async () => {
+    expect(await fs.commitFileAsync(store.get(filename))).toBe(true);
+  });
+
+  it('writes a modified file to disk', async () => {
     await fs.commitFileAsync(store.get(filename));
     expect(filesystem.readFileSync(filename).toString()).toEqual('foo');
   });
 
   it('adds non existing file to store', async () => {
-    await fs.commitFileAsync({
-      path: filenameNew,
-      contents: Buffer.from('bar'),
-      state: 'modified',
-    });
+    await fs.commitFileAsync(newFile);
     expect(store.add.callCount).toEqual(2);
+  });
+
+  it('commits non existing file', async () => {
+    expect(await fs.commitFileAsync(newFile)).toBe(true);
+  });
+
+  it("doesn't commit an unmodified file", async () => {
+    expect(
+      await fs.commitFileAsync({
+        path: filenameNew,
+        contents: 'contents',
+      })
+    ).toBe(false);
+  });
+
+  it('throws if the file is a directory', async () => {
+    filesystem.mkdirSync(filenameNew, { recursive: true });
+    await expect(fs.commitFileAsync(newFile)).rejects.toThrow();
+  });
+
+  it('throws if the directory is a file', async () => {
+    filesystem.mkdirSync(outputRoot, { recursive: true });
+    filesystem.writeFileSync(path.dirname(filenameNew), 'foo');
+    await expect(fs.commitFileAsync(newFile)).rejects.toThrow();
+  });
+
+  it('deletes a file', async () => {
+    filesystem.mkdirSync(path.dirname(filenameNew), { recursive: true });
+    filesystem.writeFileSync(filenameNew, 'foo');
+    await fs.commitFileAsync({
+      ...newFile,
+      [STATE]: STATE_DELETED,
+    });
+    expect(filesystem.existsSync(filenameNew)).toBe(false);
+  });
+
+  posixIt('set file permission', async () => {
+    await fs.commitFileAsync({
+      ...newFile,
+      stat: { mode: 0o600 },
+    });
+    // eslint-disable-next-line no-bitwise
+    expect(filesystem.statSync(filenameNew).mode & 0o777).toEqual(0o600);
+  });
+
+  posixIt("doesn't change unmodified file permission", async () => {
+    await fs.commitFileAsync({
+      ...newFile,
+      stat: { mode: 0o600 },
+    });
+
+    await fs.commitFileAsync({
+      ...newFile,
+      stat: { mode: 0o600 },
+    });
+
+    // eslint-disable-next-line no-bitwise
+    expect(filesystem.statSync(filenameNew).mode & 0o777).toEqual(0o600);
+  });
+
+  posixIt('update file permission', async () => {
+    await fs.commitFileAsync({
+      ...newFile,
+      stat: { mode: 0o600 },
+    });
+
+    await fs.commitFileAsync({
+      ...newFile,
+      stat: { mode: 0o660 },
+    });
+
+    // eslint-disable-next-line no-bitwise
+    expect(filesystem.statSync(filenameNew).mode & 0o777).toEqual(0o660);
   });
 
   it("doesn't readd same file to store", async () => {
